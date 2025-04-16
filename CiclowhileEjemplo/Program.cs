e38bb89d7827dc780d7056805c283c1a03d0dc3e
CagqDev <ca_gomez87@hotmail.com>
2025-04-16 00:00:41 -0600

    Commit de ciclo while y do-while

---
+++ CiclowhileEjemplo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace
+    UsoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //************************While*************************
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            while (i != 0)
+            {
+                Console.WriteLine($"while: {i * 10}");
+                i = Convert.ToInt32(Console.ReadLine());
+            }
+            //************************Do While*********************
+
+            int j = Convert.ToInt32(Console.ReadLine()); ;
+            do
+            {
+                Console.WriteLine($"dowhile: {j * 10}");
+                j = Convert.ToInt32(Console.ReadLine());
+
+            } while (j != 0);
+        }
+    }
+}
